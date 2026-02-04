6e89f8f6894b2622643244b249ad1418abe6134f
PhuongAnh111 <minhtai109123@gmail.com>
2026-02-04 19:37:06 +0700

    Add class creation and join class API

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,16 @@
 from flask import Flask
 from flask_cors import CORS
+
 from routes.test_routes import test_bp
+from routes.class_routes import class_bp
 
 app = Flask(__name__)
 CORS(app)
 
+# đăng ký routes
 app.register_blueprint(test_bp, url_prefix="/api/test")
+app.register_blueprint(class_bp, url_prefix="/api/classes")
 
 if __name__ == "__main__":
     app.run(debug=True)
+
---
+++ backend/routes/class_routes.py
@@ -0,0 +1,66 @@
+from flask import Blueprint, request, jsonify
+from config.database import get_db
+import random
+import string
+
+class_bp = Blueprint("class", __name__)
+
+# Hàm sinh mã lớp
+def generate_class_code(length=6):
+    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=length))
+
+#API giáo viên
+@class_bp.route("/create", methods=["POST"])
+def create_class():
+    data = request.json
+    db = get_db()
+
+    class_code = generate_class_code()
+
+    new_class = {
+        "class_name": data.get("class_name"),
+        "subject": data.get("subject"),
+        "teacher_id": data.get("teacher_id"),
+        "class_code": class_code,
+        "status": "active"
+    }
+
+    db.classes.insert_one(new_class)
+
+    return jsonify({
+        "message": "Class created successfully",
+        "class_code": class_code
+    }), 201
+#API học sinh
+@class_bp.route("/join", methods=["POST"])
+def join_class():
+    data = request.json
+    db = get_db()
+
+    class_code = data.get("class_code")
+    student_id = data.get("student_id")
+
+    classroom = db.classes.find_one({"class_code": class_code})
+
+    if not classroom:
+        return jsonify({"message": "Invalid class code"}), 404
+
+    db.class_members.insert_one({
+        "class_id": str(classroom["_id"]),
+        "student_id": student_id
+    })
+
+    return jsonify({"message": "Joined class successfully"}), 200
+#AIP lấy danh sách lớp học của giáo viên
+@class_bp.route("/student/<student_id>", methods=["GET"])
+def get_student_classes(student_id):
+    db = get_db()
+
+    memberships = db.class_members.find({"student_id": student_id})
+    class_ids = [m["class_id"] for m in memberships]
+
+    classes = list(db.classes.find({"_id": {"$in": class_ids}}, {"_id": 0}))
+
+    return jsonify(classes), 200
+
+
